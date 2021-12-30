2ae6d4c3a054fbe1db414c6a8436552e81bfce86
Varun Gungi <54449583+meenank@users.noreply.github.com>
2021-12-30 20:14:22 +0530

    output updated

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var Tbutton = document.querySelector("#btn-translate");
 var txtInp = document.querySelector("#txt-input");
+var txtOut = document.querySelector("#output");
 
 function clickRes() {
     console.log("button clicked")
     console.log(txtInp.value);
+    txtOut.innerHTML = txtInp.value;
 }
 
 Tbutton.addEventListener("click", clickRes)
