0edfb1794391d48d9d70e6e68ae080c4f968d0c7
Varun Gungi <54449583+meenank@users.noreply.github.com>
2021-12-31 13:30:03 +0530

    connected correct api and its working

--- app.js
+++ app.js
@@ -2,10 +2,74 @@ var Tbutton = document.querySelector("#btn-translate");
 var txtInp = document.querySelector("#txt-input");
 var txtOut = document.querySelector("#output");
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
 function clickRes() {
-    console.log("button clicked")
-    console.log(txtInp.value);
-    txtOut.innerHTML = txtInp.value;
+    // console.log("button clicked")
+    // console.log(txtInp.value);
+    // txtOut.innerHTML = txtInp.value;
+
+    var enteredText = txtInp.value;
+    fetch(serverUrl + "?text=" + enteredText)
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOut.innerHTML = translatedText
+        })
+
+
 }
 
 Tbutton.addEventListener("click", clickRes)
+
+/**
+ * testing api call using fetch() with mock server
+ * 
+ * var txt1 = "i am ironman"
+ * var txt2 = "i am batman"
+ * var txt3 = "bring me thanos"
+ * 
+ * mock server : https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
+ * 
+ * function apcall(text)
+ *  {fetch(url+"?text="+text).then(Response => Response.json()).then(json => console.log(json))}
+ * 
+ * call 1 : apcall(txt1)
+ * response : 
+ *    {
+    "success": {
+        "total": 1
+    },
+    "contents": {
+        "translated": "Testing,  you are",
+        "text": "i am ironman",
+        "translation": "yoda"
+    }
+    }
+ *  
+    call 2 : apcall(txt2)
+ * response : 
+ *    {
+    "success": {
+        "total": 1
+    },
+    "contents": {
+        "translated": "Testing,  you are",
+        "text": "i am batman",
+        "translation": "yoda"
+    }
+    }
+
+    call 3 : apcall(txt3)
+ * response : 
+ *    {
+    "success": {
+        "total": 1
+    },
+    "contents": {
+        "translated": "Testing,  you are",
+        "text": "bring me thanos",
+        "translation": "yoda"
+    }
+    }
+ *  */
